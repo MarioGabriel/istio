64a598ca9311b281f70632e907f15573ce104ddf
Mario Gabriel GUAZZELLI <mg.guazzelli@sentelis.com>
2019-07-02 15:23:22 +0200

    fixed ctx.sh

--- config/ctx.sh
+++ config/ctx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-eksctl utils write-kubeconfig --name=test-cluster
-gcloud container clusters get-credentials gke-local-cluster --zone europe-west1-b --project istiotest-239415
+eksctl utils write-kubeconfig --name=test-cluster -v 0
+gcloud container clusters get-credentials gke-local-cluster --zone europe-west1-b --project istiotest-239415 --no-user-output-enabled
 
 eks=$(kubectl config get-contexts -o='name' | grep 'eks')
 gke=$(kubectl config get-contexts -o='name' | grep 'gke')
